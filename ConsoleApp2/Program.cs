e72a07edc3b677b6ce90bef53fd7b8c26603de3a
Bill Napier <bill.napier@core10.io>
2019-08-03 16:22:04 -0400

    adding async example

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -26,12 +26,25 @@ namespace ConsoleApp2
             //fb2.WriteAnswer();
 
             //Alternative solution 3.
-            FizzBuzz3 fb3 = new FizzBuzz3();
-            fb3.WriteAnswer();
+            //FizzBuzz3 fb3 = new FizzBuzz3();
+            //fb3.WriteAnswer();
 
+            //Alternative solution 4.
+            //FizzBuzz4 fb4 = new FizzBuzz4();
+            //fb4.WriteAnswer();
 
+            //FizzBuzz5 fb5 = new FizzBuzz5();
+            //fb5.WriteAnswerFromMemory();
+            //fb5.WriteAnswerFromMemory2();
+            //fb5.WriteAnswerFromMemory3();
+            //fb5.WriteAnswerFromMemory4();
 
+            Console.WriteLine("Calling sync function ...");
+            Async1 a1 = new Async1();
+            a1.RunSyncFunc();
 
+            Console.WriteLine("Calling async function ...");
+            a1.RunAsyncFunc();
 
             //Stop, collaborate and listen!
             //Ice is back with a brand new invention.
@@ -52,8 +65,13 @@ namespace ConsoleApp2
             }
             return collection;
         }
-    }
 
+        public static List<int> CreateEnumCollection()
+        {
+            List<int> collection = Enumerable.Range(1, 100).ToList();
+            return collection;
+        }
+    }
 
     class FizzBuzz1
     {
@@ -140,6 +158,168 @@ namespace ConsoleApp2
                     Console.Write(Environment.NewLine);
                 }
             }
+        }
+    }
+
+    class FizzBuzz4
+    {
+        public void WriteAnswer()
+        {
+            var collection = CollectionService.CreateEnumCollection();
+
+            foreach (var i in collection)
+            {
+                string bugger = null;
+
+                if (i % 3 == 0)
+                {
+                    bugger += "Fizz";
+                }
+
+                if (i % 5 == 0)
+                {
+                    bugger += "Buzz";
+                }
+
+                if (bugger != null)
+                {
+                    Console.WriteLine($"{i}: {bugger}");
+                }
+            }
+        }
+    }
+
+    class FizzBuzz5
+    {
+        public void WriteAnswerFromMemory()
+        {
+            List<int> collection = Enumerable.Range(1, 100).ToList();
+
+            foreach (var x in collection)
+            {
+                if (x % 3 == 0)
+                {
+                    Console.WriteLine(x + ". Fizz");
+                }
+
+                if (x % 5 == 0)
+                {
+                    Console.WriteLine(x + ". Buzz");
+                }
+
+                if ((x % 3 == 0) && (x % 5 == 0))
+                {
+                    Console.WriteLine(x + ". FizzBuzz");
+                }
+            }
+        }
+
+        public void WriteAnswerFromMemory2()
+        {
+            List<int> collection = Enumerable.Range(1, 100).ToList();
+
+            foreach (int x in collection)
+            {
+                if (x % 3 == 0)
+                {
+                    Console.Write("Fizz");
+                }
+
+                if (x % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                }
+            }
+        }
+
+        public void WriteAnswerFromMemory3()
+        {
+            List<int> collection = Enumerable.Range(1, 100).ToList();
+
+            foreach (int x in collection)
+            {
+                bool match = false;
+
+                if (x % 3 == 0)
+                {
+                    Console.Write($"{x}. Fizz");
+                    match = true;
+                }
+
+                if (x % 5 == 0)
+                {
+                    if (match)
+                    {
+                        Console.Write("Buzz");
+                    }
+                    else
+                    {
+                        Console.Write($"{x}. Buzz");
+                    }
+                    match = true;
+                }
+
+                if (match)
+                {
+                    Console.Write(Environment.NewLine);
+                }
+            }
+        }
+
+        public void WriteAnswerFromMemory4()
+        {
+            List<int> collection = Enumerable.Range(1, 100).ToList();
+
+            foreach (int x in collection)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                if (x % 3 == 0)
+                {
+                    sb.Append("Fizz");
+                }
+
+                if (x % 5 == 0)
+                {
+                    sb.Append("Buzz");
+                }
+
+                if (!String.IsNullOrEmpty(sb.ToString()))
+                {
+                    Console.WriteLine($"{x}. {sb.ToString()}");
+                }
+            }
+        }
+    }
+
+    class Async1
+    {
+        int ComplexCalculation()
+        {
+            double x = 2;
+            for (int i = 1; i < 100000000; i++)
+            {
+                x += Math.Sqrt(x) / i;
+
+            }
+            return (int)x;
+        }
+
+        public void RunSyncFunc()
+        {
+            int result = ComplexCalculation();
+            Console.WriteLine(result);
+        }
+
+        Task<int> ComplexCalculationAsync()
+        {
+            return Task.Run(() => ComplexCalculation());
+        }
+
+        public async void RunAsyncFunc()
+        {
+            int result = await ComplexCalculationAsync();
+            Console.WriteLine(result);
         }
     }
 }
