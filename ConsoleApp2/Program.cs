e8e11c080408aaeaabd5f2ecd154111b639c7911
Bill Napier <bill.napier@core10.io>
2019-08-03 06:43:31 -0400

    add ConsoleApp2

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //The problem
+            //You have a collection of integers, 1 to 100. I want you to cycle through this collection.
+            //For each number found that is evenly divisible by 3, output the word "Fizz". For each
+            //number that is evenly divisible by 5, output the word "Buzz". For each number that is
+            //evenly divisble by both 3 and 5 output the word "FizzBuzz".
+
+            // Basic solution 1.
+            FizzBuzz1 fb1 = new FizzBuzz1();
+            fb1.WriteAnswer();
+
+            //Alternative solution 2.
+
+
+            Console.ReadLine();
+        }
+    }
+
+    static class CollectionService
+    {
+        public static List<int> CreateCollection()
+        {
+            List<int> collection = new List<int>();
+            for (int i = 1; i <= 100; i++)
+            {
+                collection.Add(i);
+            }
+            return collection;
+        }
+    }
+    
+
+    class FizzBuzz1
+    {
+        public void WriteAnswer()
+        {
+            var collection = CollectionService.CreateCollection();
+
+            foreach (int i in collection)
+            {
+                if (i % 3 == 0)
+                    Console.WriteLine("Fizz. i is " + i);
+
+                if (i % 5 == 0)
+                    Console.WriteLine("Buzz. i is " + i);
+
+                if ((i % 3 == 0) && (i % 5 == 0))
+                        Console.WriteLine("FizzBuzz. i is " + i);
+            }
+        }
+    }
+
+    class FizzBuzz2
+    {
+        //public void WriteAnswer()
+    }
+}
