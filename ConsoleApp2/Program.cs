626ddedd85274ac388d64e37e13c4a4596c66e2e
Bill Napier <bill.napier@core10.io>
2019-08-03 07:32:47 -0400

    add fizzbuzz solution 3

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -25,6 +25,10 @@ namespace ConsoleApp2
             //FizzBuzz2 fb2 = new FizzBuzz2();
             //fb2.WriteAnswer();
 
+            //Alternative solution 3.
+            FizzBuzz3 fb3 = new FizzBuzz3();
+            fb3.WriteAnswer();
+
 
 
 
@@ -60,13 +64,19 @@ namespace ConsoleApp2
             foreach (int i in collection)
             {
                 if (i % 3 == 0)
+                {
                     Console.WriteLine("Fizz. i is " + i);
+                }
 
                 if (i % 5 == 0)
+                {
                     Console.WriteLine("Buzz. i is " + i);
+                }
 
                 if ((i % 3 == 0) && (i % 5 == 0))
+                {
                     Console.WriteLine("FizzBuzz. i is " + i);
+                }
             }
         }
     }
@@ -80,10 +90,55 @@ namespace ConsoleApp2
             foreach (int i in collection)
             {
                 if (i % 3 == 0)
+                {
                     Console.Write("Fizz");
+                }
 
                 if (i % 5 == 0)
+                {
                     Console.Write("Buzz");
+                }
+            }
+        }
+    }
+
+    class FizzBuzz3
+    {
+        public void WriteAnswer()
+        {
+            var collection = CollectionService.CreateCollection();
+
+            foreach (int i in collection)
+            {
+                bool insertNewline = false;
+
+                if (i % 3 == 0)
+                {
+                    //int is divisible by 3, so write Fizz.
+                    Console.Write("Fizz");
+
+                    //Insert a line break is true.
+                    insertNewline = true;
+                }
+
+                if (i % 5 == 0)
+                {
+                    //int is divisible by 5, so write Buzz.
+                    //If int is divisible by 3 and 5, this will write FizzBuzz.
+                    Console.Write("Buzz");
+
+                    //Insert a line break is true.
+                    //Note, if divisible by 3 and 5 insertNewline is already true.
+                    //This sets it to true again ... which is OK, I guess.
+                    insertNewline = true;
+                }
+
+                if (insertNewline)
+                {
+                    //If insert line break == true, then we insert one.
+                    //This gives us Fizz, Buzz or FizzBuzz.
+                    Console.Write(Environment.NewLine);
+                }
             }
         }
     }
