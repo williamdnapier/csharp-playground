aff450b959b41b748b10d31cfb2fe270d2c0e5d2
Bill Napier <napier.bill@gmail.com>
2019-08-16 17:52:38 -0400

    adding unit tests

---
+++ 021-NunitCh2.Tests.2/LogAnalyzerTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidFileName_validFileLowerCased_ReturnsTrue()
+        {
+            //Arrange
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            //Act
+            bool result = analyzer.isValidLogFileName("whatever.slf");
+
+            //Assert
+            Assert.IsTrue(result, "filename should be valid");
+        }
+    }
+}
---
+++ 021-NunitCh2/LogAnalyzer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class LogAnalyzer
+{
+    public bool isValidLogFileName(string fileName)
+    {
+        if(!fileName.EndsWith(".slf", StringComparison.Ordinal))
+        {
+            return false;
+        }
+        return true;
+    }
+}
