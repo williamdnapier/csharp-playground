c3bfc9b2f73c7b334c9cbbdb44f2aadf7da6dab7
Bill Napier <bill.napier@core10.io>
2019-08-20 14:16:44 -0400

    update nunit tests

---
+++ 025-MyApp.Tests/MyMathTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace _025_MyApp.Tests
+{
+    [TestFixture]
+    public class MyMathTests
+    {
+        [Test]
+        public void MyAddTest()
+        {
+            //Arrange
+            MyMath myMath = new MyMath();
+
+            //Act
+
+            //Assert
+            Assert.AreEqual(myMath.Add(2, 3), 5);
+        }
+
+        [Test]
+        public void MyObjTest()
+        {
+            //Arrange
+            MyMath myMath = new MyMath();
+
+            //Act
+            object o1 = myMath.Obj;
+            object o2 = myMath.Obj2;
+
+            //Assert
+            Assert.AreSame(o1, o2);
+        }
+
+        //[Test]
+        //public void MyFailedTest()
+        //{
+        //    //Arrange
+        //    MyMath myMath = new MyMath();
+
+        //    //Act
+
+        //    //Assert
+        //    Assert.Fail("This test is intentionally failing!");
+        //}
+    }
+}
---
+++ 025-MyApp/MyMath.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _025_MyApp
+{
+    public class MyMath
+    {
+        public int Add(int i, int j)
+        {
+            return i + j;
+        }
+
+        private object mObj;
+        public object Obj
+        {
+            get => mObj;
+            set => mObj = value;
+        }
+        public object Obj2
+        {
+            get => mObj;
+            set => mObj = value;
+        }
+        public MyMath()
+        {
+
+        }
+    } 
+}
