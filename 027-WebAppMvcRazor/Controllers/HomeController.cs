61584a28ea7d8f06e5bda62c1da2fbee9d287396
Bill Napier <bill.napier@core10.io>
2019-08-22 15:13:41 -0400

    updating razor project

--- 027-WebAppMvcRazor/Controllers/HomeController.cs
+++ 027-WebAppMvcRazor/Controllers/HomeController.cs
@@ -22,5 +22,51 @@ namespace _027_WebAppMvcRazor.Controllers
         {
             return View(myProduct);
         }
+
+        public ActionResult NameAndPrice()
+        {
+            return View(myProduct);
+        }
+
+        public ViewContext MyViewContext()
+        {
+            return new ViewContext();
+        }
+
+        public ActionResult DemoExpression()
+        {
+            ViewBag.ProductCount = 1;
+            ViewBag.ExpressShip = true;
+            ViewBag.ApplyDiscount = false;
+            ViewBag.Supplier = null;
+
+            return View(myProduct);
+        }
+
+        public ActionResult DemoArray()
+        {
+            Product[] array =
+            {
+                new Product { },
+                new Product { },
+                new Product { },
+                new Product { }
+            };
+
+            return View(array);
+        }
     }
+
+    //Discussion
+    //In a good MVC Framework application, there is a clear separation
+    //between the roles that the action method and view perform. The
+    //rules are simple:
+
+    //ActionMethod
+    //It passes a view model object to the view.
+    //It does NOT pass formatted data to the view.
+
+    //View
+    //It uses the view model object to present content to the user.
+    //It does NOT change any aspect of the view model object.
 }
