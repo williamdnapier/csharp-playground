3b60ba42082010d6cebb2a9a178708a5af678749
William Napier <william.napier@uspsector.com>
2020-04-16 20:16:09 -0400

    update, add 030-Async

---
+++ 030-Async/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _030_Async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadTest.DoThreading();
+            TaskTest.DoTasking();
+            Console.ReadLine();
+        }  
+    }
+
+    class ThreadTest
+    {
+        public static void DoThreading()
+        {
+            Thread t = new Thread(WriteY);  //Kicking off a new thread
+            t.Start();                      //Running WriteY()
+
+            for (int i = 0; i < 1000; i++)  //Simultaneously, do something on the main thread.
+            {
+                Console.Write("x");
+            }
+        }
+
+        public static void WriteY()
+        {
+            Console.WriteLine(Thread.CurrentThread.Name);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write("y");
+            }
+        }
+    }
+
+    class TaskTest
+    {
+        public static void DoTasking()
+        {
+            Task.Run(() => Console.WriteLine("Foo"));
+
+            Action a = GetAction();
+
+            Task.Run(a);
+        }
+
+        public static Action GetAction()
+        {
+            return () => Console.WriteLine("Bar");
+        }
+    }
+}
